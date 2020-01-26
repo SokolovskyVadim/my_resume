7d2d24457a8cdd7799ca8138b845a7fc0cd5212b
SokolovskyVadim <sokolovsky.v.a@gmail.com>
2020-01-26 22:29:21 +0300

    feat: added 2 new blocks

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ jQuery(document).ready(function($) {
         $('#preloader').delay(350).fadeOut('slow');
     }) 
 });
-
 jQuery(document).ready(function($) { 
     $(document).ready(function(){
         $(".smooth_menu").on("click","a", function (event) {
@@ -15,7 +14,6 @@ jQuery(document).ready(function($) {
         });
     });
 });
-
 jQuery(document).ready(function($) {  
     $(window).scroll(function() {
         var height = $(window).scrollTop();
@@ -28,7 +26,6 @@ jQuery(document).ready(function($) {
 
     });
 });
-
 jQuery(document).ready(function($) {  
     $(window).scroll(function() {
         var height = $(window).scrollTop();
@@ -41,15 +38,22 @@ jQuery(document).ready(function($) {
 
     });
 });
+jQuery(document).ready(function($) {  
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        // var box1 = $('.about_me_block_wrapper').offset().top;
+        if(height > 700){
+        $('.working_gif').addClass('zoomIn');
+        } else{
+        $('.working_gif').removeClass('zoomIn');
+        }
 
-
-
+    });
+});
 jQuery(document).ready(function($) {
-
     $(window).bind('scroll',function(e){
         parallaxScroll();
     });
-
     function parallaxScroll(){
         var scrolled = $(window).scrollTop();
         $('.parralax-to-top-item').css('top',(0-(scrolled*.7))+'px');
@@ -57,4 +61,21 @@ jQuery(document).ready(function($) {
         $('.parralax-to-top-item3').css('top',(0-(scrolled*.2))+'px');
         $('.parralax-to-top-item4').css('top',(0-(scrolled*.8))+'px');
     }
+});
+jQuery(document).ready(function($) {
+    $('.card').click(function(){
+        $(this).toggleClass('flipped');
+    });
+});
+
+jQuery(document).ready(function($) {
+    $(".fancybox").fancybox({
+        beforeShow : function() {
+            var alt = this.element.find('img').attr('alt');
+            
+            this.inner.find('img').attr('alt', alt);
+            
+            this.title = alt;
+        }
+    });
 });
